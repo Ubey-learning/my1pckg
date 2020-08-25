8635dd6ca6db9be4402860098af4a9aed207f0b4
Ubey-learning <70204706+Ubey-learning@users.noreply.github.com>
2020-08-25 13:01:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
+
 # my1pckg
-“My first R package :)”
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of my1pckg is to ...
+
+## Installation
+
+You can install the released version of my1pckg from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("my1pckg")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(my1pckg)
+## basic example code
+```
+
---
+++ my1pckg/R/Funs.R
@@ -0,0 +1,34 @@
+#' The sum !
+#'
+#' @description
+#' This **wonderful** function will calculate for you the sum of any two numerical vectors.
+#'
+#' @param x,y `Numeric` vectors.
+#' @param plot `Logical` default *FALSE*
+#' @return `Numeric` vector of `x`, `y`, and `x+y`.
+#' @return `plot` if *plot = TRUE*
+#'
+#' @examples
+#' # sum of two numbers
+#' add(1, 1)
+#'
+#' # sum of two vectors
+#' add(c(1, 2), 1:8)
+#' @details
+#' The calculation of \eqn{x+y} is obvious. No details are needed!
+#'
+#' @seealso
+#' For more details see <https://en.wikipedia.org/wiki/Summation>.
+#'
+#' @export
+#'
+
+add <- function(x, y, plot = FALSE) {
+  if (!plot) {
+    return(c(x, y, x + y))
+  } else if (length(x) == length(y)) {
+    print(print(ggplot2::qplot(x,y)))
+  }
+  return(c(x, y, x + y))
+
+}
